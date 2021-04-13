b575b04b87d9ca90f82611089f51e53b8210ec7c
maxt0214 <49177628+maxt0214@users.noreply.github.com>
2021-04-13 00:55:03 -0400

    Added Manager Spawner
    Manager Spawner ensure needed managers to be singly instanced across all scenes

---
+++ Src/Client/Assets/Scripts/Managers/ManagerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Network;
+
+public class ManagerSpawner : MonoBehaviour {
+    [Header("Manager Prefabs")]
+    public Transform netClient;
+    public Transform sceneManager;
+
+    void Awake()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadMode)
+    {
+        if (!NetClient.Instance)
+        {
+            Instantiate(netClient, transform.position, transform.rotation);
+        }
+
+        if(!SceneManager.Instance)
+        {
+            Instantiate(sceneManager, transform.position, transform.rotation);
+        }
+    }
+}
